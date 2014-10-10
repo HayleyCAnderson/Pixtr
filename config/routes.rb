4e303fc9e3d46fe13786986caca5f520f8a2deb2
Hayley <hayleyanderson@zoho.com>
2014-10-10 15:45:53 -0400

    Add monban and improve layout *Add monban and run generators *Add home button to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
+require "monban/constraints/signed_in"
+require "monban/constraints/signed_out"
+
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   root "galleries#index"
   
   resources :galleries do
     resources :images, except: :index do
     end
+  end
+
+  constraints Monban::Constraints::SignedIn.new do 
+  end
+
+  constraints Monban::Constraints::SignedOut.new do
   end
 end
